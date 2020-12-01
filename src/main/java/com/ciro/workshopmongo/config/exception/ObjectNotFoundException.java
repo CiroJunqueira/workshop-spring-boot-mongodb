d449e3299ee68e59614e0cb3415212a41e32f251
Ciro Junqueira <cirojunqueira1@gmail.com>
2020-12-01 19:11:18 -0300

    Obtendo usuario por id

---
+++ src/main/java/com/ciro/workshopmongo/config/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.ciro.workshopmongo.config.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException (String msg) {
+		super(msg);
+	}
+
+}
--- src/main/java/com/ciro/workshopmongo/repository/services/UserService.java
+++ src/main/java/com/ciro/workshopmongo/repository/services/UserService.java
@@ -1,10 +1,12 @@
 package com.ciro.workshopmongo.repository.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ciro.workshopmongo.config.exception.ObjectNotFoundException;
 import com.ciro.workshopmongo.domain.User;
 import com.ciro.workshopmongo.repository.UserRepository;
 
@@ -18,5 +20,11 @@ public class UserService {
 		return repo.findAll();
 		
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 
 }
