f6cb0861363f71693425fb12d62a1f1a245fb259
Ciro Junqueira <cirojunqueira1@gmail.com>
2020-12-02 22:31:00 -0300

    Obtendo post por id

---
+++ src/main/java/com/ciro/workshopmongo/repository/services/PostService.java
@@ -0,0 +1,29 @@
+package com.ciro.workshopmongo.repository.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ciro.workshopmongo.config.exception.ObjectNotFoundException;
+import com.ciro.workshopmongo.domain.Post;
+import com.ciro.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+		
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	
+}
